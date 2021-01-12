2be23eb2864b3fc1031a25260e2f14ed24c01a84
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-01-12 16:19:13 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
         slivers: [
           SliverAppBar(
             expandedHeight: 200.0,
+            pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               title: Text(
                 title,
@@ -42,10 +43,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               centerTitle: true,
-              // background: Image.asset(
-              //   "assets/images/gi_digital.jpg",
-              //   fit: BoxFit.cover,
-              // ),
+              background: Image.asset(
+                "assets/images/gi_digital2.png",
+                width: 30,
+                height: 30,
+                // fit: BoxFit.fill,
+              ),
             ),
             leading: IconButton(
               onPressed: () {},
@@ -60,12 +63,80 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           SliverFillRemaining(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  margin: EdgeInsets.only(top: 5),
-                  child: Text("hello world"),
-                )
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Flexible(
+                          flex: 1,
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            color: Colors.red,
+                            child: Column(
+                              children: [
+                                Text(
+                                  "GEMA INSANI DIGITAL",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                                Text(
+                                  "Islamic Digital Book Publisher",
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      child: Image.asset(
+                                        "assets/images/Google-Play.png",
+                                        width: 80,
+                                        height: 40,
+                                      ),
+                                    ),
+                                    Container(
+                                      child: Image.asset(
+                                        "assets/images/Google-Play-Books.png",
+                                        width: 80,
+                                        height: 40,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )),
+                      Flexible(
+                          flex: 1,
+                          child: Container(
+                            margin: EdgeInsets.all(5),
+                            color: Colors.purple,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Image.asset(
+                                  "assets/images/mockup-mobile.png",
+                                  fit: BoxFit.fill,
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+                Flexible(
+                    flex: 2,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.amber,
+                    )),
+                Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.blue,
+                    ))
               ],
             ),
           )
